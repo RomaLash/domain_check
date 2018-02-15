92e99e5e756733ebb534c483168b9ccb3184f21c
bibi\Lash <lash684@gmail.com>
2018-02-15 14:36:53 +0300

    v 0.5

--- check_domain.py
+++ check_domain.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Программа сканирует определенные порты в диапазоне заданных ip-адресов.
-В начале создается массив всех адресов (адрес * диапазон).
-Затем три потока обрабатывают этот массив * порты.
-Для открытых портов 80 и 433 программа получает request.headres(), содержащий информацию о сервере.
+Программа осуществляет поиск доменов, схожих с заданным, используя несколько стратегий.
+Проверка домена осуществляется путем попытки получить ip-адрес функцией socket.gethostbyname()
 """
 import threading
 import socket as soc
@@ -41,39 +39,78 @@ class WorkThread(threading.Thread):
         return next_adrr
 
     def check_domain(self, next_addr):
+        """
+        проверка домена 
+        :param next_addr: следущий адрес из списка 
+        """
         try:
-            sock = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
-            sock.settimeout(0.01)
-            sock.gethostbyname(next_addr)
+            soc.gethostbyname(next_addr)
             print(self.name, next_addr, ' - is available')
-            sock.close()
         except:
             pass
 
 
 class Strategies():
-    def __init__(self, domain, zone_list):
+    def __init__(self, domain):
         self.domain = domain
-        self.zone_list = zone_list
 
-    def adding(self, alfabet):
+    def adding(self, alphabet):
+        """
+        Добавляет в список доменов для проверки домены, которые получаются при добавлении одного символа в конец строки.
+        Можно выбрать, какие символы добавлять
+        :param alphabet: выбор добавляемых символом. дефолт - все буквы английского алфавита в лоукэйс
+        :return: список доменов для проверки
+        """
         domain_add_list = []
-        for letter in alfabet:
+        for letter in alphabet:
             domain_add_list.append(self.domain + letter)
         return domain_add_list
 
-    def
+    def subdomain(self):
+        """
+        Добавляет в список доменов для проверки поддомены, получающиеся при подставлении точки в изначальный домен.
+        :return: список доменов для проверки
+        """
+        domain_sub_list = []
+        i = 0
+        while i < len(self.domain):
+            domain_sub_list.append(self.domain[0:i] + '.' + self.domain[i:])
+            i += 1
+        return domain_sub_list
+
+    def deleting(self):
+        """
+        Добавляет в список доменов для проверки домены, которые получаются путем удаления одного символа из изначального домена.
+        :return: список доменов для проверки
+        """
+        domain_del_list = []
+        i = 0
+        while i < len(self.domain):
+            domain_del_list.append(self.domain[:i] + self.domain[i+1:])
+            i += 1
+        return domain_del_list
 
 
 if __name__ == "__main__":
-    address = input('Enter address (0.0.0.): ')
-    diap = int(input('Enter diap (0): '))
-    ports = re.split(' ', input('Enter ports: (0 0 0))'))
+    domain = input('Enter domain: ')
+    choose = input('Choose strategy: \n 1.Adding \n 2.Subdomain \n 3.Deleting \n')
+    zone_list = ['com', 'ru', 'net', 'org', 'info', 'cn', 'es', 'top', 'au', 'pl', 'it', 'uk', 'tk', 'ml', 'ga', 'cf', 'us', 'xyz', 'top', 'site', 'win', 'bid']
+    domain_list = []
+    if choose == '1':
+        s = Strategies(domain)
+        domain_list = s.adding(string.ascii_lowercase)
+    elif choose == '2':
+        s = Strategies(domain)
+        domain_list = s.subdomain()
+    elif choose == '3':
+        s = Strategies(domain)
+        domain_list = s.deleting()
     address_list = []
-    for a in range(diap):
-        address_list.append(address + str(a))
-    # print("list: \n", address_list)
+    for zone in zone_list:
+        for dom in domain_list:
+            address_list.append(dom + '.' + zone)
+    print(address_list)
     addrs_lock = threading.Lock()
     for x in range(0, 3):
-        newthread = WorkThread(address_list, ports, addrs_lock)
+        newthread = WorkThread(address_list, addrs_lock)
         newthread.start()
